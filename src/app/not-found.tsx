1f50f4d08c02860e1d521ba4bf55b663e6f14445
Peter Khalil <peter.khalil.fahmy@gmail.com>
2024-10-28 15:28:13 +0300

    🚨 Linting

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -6,7 +6,7 @@ const NotFound = () => {
     document.title = 'Not found';
   }, []);
   return (
-    <div className="flex h-[100svh] w-full flex-col items-center justify-center gap-5 bg-transparent">
+    <div className="flex h-svh w-full flex-col items-center justify-center gap-5 bg-transparent">
       <h1 className="text-4xl font-bold">404</h1>
       <p className="text-lg">Page not found</p>
     </div>
